ad9cde52daffdadbf204677307d0ee78b4e43c2a
Leonardo Vilches <leovilches08@gmail.com>
2021-02-10 11:12:20 -0300

    first commit

---
+++ src/main/java/com/lv08/smartstockmongo/SmartstockmongoApplication.java
@@ -0,0 +1,13 @@
+package com.lv08.smartstockmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartstockmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartstockmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lv08/smartstockmongo/SmartstockmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lv08.smartstockmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartstockmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
